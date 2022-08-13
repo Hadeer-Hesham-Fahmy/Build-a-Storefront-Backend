49502a082da7fe432dcdf85f87f71390047c999d
Hadeer Hesham Fahmy <heidyhesham3@gmail.com>
2022-08-13 13:01:57 +0200

    Create server.ts

---
+++ src/server.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import productRoutes from './routes/product-route';
+import userRoutes from './routes/user-route';
+import orderRoutes from './routes/order-route';
+import config from './config';
+
+const app: express.Application = express();
+const corsOptions = {
+  origin: 'http://someotherdomain.com',
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+
+app.use('/', productRoutes);
+app.use('/', userRoutes);
+app.use('/', orderRoutes);
+
+app.get('/', function (_req: Request, res: Response) {
+  res.send('Hello World!');
+});
+
+const port = config.port || 8000;
+
+app.listen(port, function () {
+  console.log(`starting app on http://localhost:${port}`);
+});
+
+export default app;
