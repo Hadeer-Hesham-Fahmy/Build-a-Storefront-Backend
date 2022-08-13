1aa4ff03c900093a7e65150f41905207a162176a
Hadeer Hesham Fahmy <heidyhesham3@gmail.com>
2022-08-13 13:01:24 +0200

    Create database.ts

---
+++ src/database.ts
@@ -0,0 +1,11 @@
+import { Pool } from 'pg';
+import config from './config';
+
+const pool = new Pool({
+  host: config.host,
+  database: config.database,
+  user: config.user,
+  password: config.password,
+});
+
+export default pool;
